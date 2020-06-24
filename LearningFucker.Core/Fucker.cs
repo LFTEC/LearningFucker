55df6919eb8d0c779b64034a857c0aeec8418dc7
chris jiang <jiangyao@whchem.com>
2020-06-24 15:09:54 +0800

    date: 2020-6-24 端口号从4443变更为6443

--- LearningFucker.Core/Fucker.cs
+++ LearningFucker.Core/Fucker.cs
@@ -17,7 +17,7 @@ namespace LearningFucker
         public Fucker(Worker worker)
         {
             httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri("https://learning.whchem.com:4443");
+            httpClient.BaseAddress = new Uri("https://learning.whchem.com:6443");
             httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla /5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1");
 
             this.worker = worker;
